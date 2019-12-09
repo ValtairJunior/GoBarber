65f52d6d6ca43d1523c6280cb8e7477731a48cc9
Asrlopes <a.junior.srlopes@gmail.com>
2019-12-08 22:44:34 -0400

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    calmelcase: "off",
-    "no-unused-vars": ["error", { argsIngorePattern: "next" }]
-  }
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    calmelcase: 'off',
+    'no-unused-vars': ['error', { argsIngorePattern: 'next' }],
+  },
 };
