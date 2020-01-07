d87299f82f19baacaf011d01c640b92732e25207
Asrlopes <a.junior.srlopes@gmail.com>
2020-01-07 15:24:52 -0400

    loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'aba@gnail.com',
+    password_hash: '111111',
+  });
+  return res.json(user);
+});
 
 export default routes;
