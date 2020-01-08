7fd45f262bd9a06b95dd4dd14a5c2084c92e128b
Asrlopes <a.junior.srlopes@gmail.com>
2020-01-08 17:46:24 -0400

    config avatar usuario

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/user';
+import File from '../app/models/file';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -14,6 +15,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
