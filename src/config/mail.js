f5fd08fb54d8aaed2f44fa5c8f1928f622d461be
Asrlopes <a.junior.srlopes@gmail.com>
2020-01-13 17:54:55 -0400

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'afc3dd55ffcd04',
+    pass: 'a25d0ae91d73ff',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
